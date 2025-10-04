87e09e13e6fc6362f49fc17cf8cce88bfc414461
mohamed <137658373+alarabyy@users.noreply.github.com>
2025-10-04 21:46:53 +0300

    add first file 2

---
+++ src/app/components/Courses/edit-courses/edit-courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-courses.component.html',
+  styleUrl: './edit-courses.component.scss'
+})
+export class EditCoursesComponent {
+
+}
---
+++ src/app/components/Courses/list-of-courses/list-of-courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-of-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-of-courses.component.html',
+  styleUrl: './list-of-courses.component.scss'
+})
+export class ListOFCoursesComponent {
+
+}
